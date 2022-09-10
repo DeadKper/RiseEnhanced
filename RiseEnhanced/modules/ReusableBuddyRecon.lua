45072ec1a8f10e6588918b95bc2aa876d49ba2f2
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2022-09-10 16:23:02 -0600

    add new modules

---
+++ RiseEnhanced/modules/ReusableBuddyRecon.lua
@@ -0,0 +1,96 @@
+local module = {
+	folder = "Reusable Buddy Recon",
+}
+
+local config
+local modUtils
+local settings
+
+-- infinite_buddy_recon.lua : written by arcwizard1204
+
+local travelCount
+local OtomoReconManager
+local multiplier
+
+local function on_pre_onCompleteReconOtomoAct(args)
+	if settings.data.enable then
+		return sdk.PreHookResult.SKIP_ORIGINAL
+	else
+		return sdk.PreHookResult.CALL_ORIGINAL
+	end
+end
+
+local function on_post_onCompleteReconOtomoAct(retval)
+	if settings.data.enable then
+		sdk.get_managed_singleton("snow.otomo.OtomoManager")._RefOtReconManager:removeReconOtomo()
+	end
+    return retval
+end
+
+local function on_pre_showReconOtomo(args)
+	if settings.data.enable then
+		travelCount = travelCount + 1
+	end
+	return sdk.PreHookResult.CALL_ORIGINAL
+end
+
+local function on_pre_initRewardList(args)
+	if settings.data.enable then
+		local usedPoints = settings.data.cost * multiplier * travelCount
+		if travelCount > 0 then
+			OtomoReconManager:set_field("_IsUseOtomoReconFastTravel", true)
+		end
+		OtomoReconManager:set_field("UseOtomoReconFastTravelVillagePoint", usedPoints)
+	end
+
+	return sdk.PreHookResult.CALL_ORIGINAL
+end
+
+local function on_pre_initQuestStart(args)
+	OtomoReconManager = sdk.get_managed_singleton("snow.data.OtomoReconManager")
+	if settings.data.enable then
+		if travelCount > 0 then
+			travelCount = 0
+			OtomoReconManager:set_field("_IsUseOtomoReconFastTravel", false)
+		end
+		OtomoReconManager:set_field("UseOtomoReconFastTravelVillagePoint", settings.data.cost * multiplier)
+	end
+
+	return sdk.PreHookResult.CALL_ORIGINAL
+end
+
+function module.init()
+	config = require "RiseEnhanced.misc.config"
+	modUtils = require "RiseEnhanced.utils.mod_utils"
+	multiplier = 10
+	settings = modUtils.getConfigHandler({
+		enable = true,
+		cost = 100 / multiplier,
+	}, config.folder .. "/" .. module.folder)
+
+	travelCount = 0
+	OtomoReconManager = nil
+
+	sdk.hook(sdk.find_type_definition("snow.otomo.OtomoReconCharaManager"):get_method("onCompleteReconOtomoAct"), on_pre_onCompleteReconOtomoAct, on_post_onCompleteReconOtomoAct)
+
+	sdk.hook(sdk.find_type_definition("snow.otomo.OtomoReconCharaManager"):get_method("showReconOtomo"), on_pre_showReconOtomo)
+
+	sdk.hook(sdk.find_type_definition("snow.gui.GuiQuestResultFsmManager"):get_method("initRewardList"), on_pre_initRewardList)
+
+	sdk.hook(sdk.find_type_definition("snow.SnowSessionManager"):get_method("initQuestStart"),
+		on_pre_initQuestStart)
+end
+
+function module.draw()
+	if imgui.tree_node(config.lang.buddyRecon.name) then
+		settings.imgui("enable", imgui.checkbox, config.lang.enable)
+		settings.imgui("cost", imgui.slider_int, config.lang.buddyRecon.cost, 0, 1000 / multiplier, settings.data.cost * multiplier)
+		if imgui.button(config.lang.reset) then
+            settings.update(true, "enable")
+			settings.update(math.floor(100 / multiplier), "cost")
+        end
+		imgui.tree_pop()
+	end
+end
+
+return module
---
+++ RiseEnhanced/modules/Skips.lua
@@ -0,0 +1,121 @@
+local module = {
+	folder = "Skip Intro Logos & Online Warnings",
+}
+
+local config
+local modUtils
+local settings
+
+local FINISHED
+local LOADING_STATES
+
+local function isLoading()
+	local GuiGameStartFsmManager = sdk.get_managed_singleton("snow.gui.fsm.title.GuiGameStartFsmManager")
+	if GuiGameStartFsmManager then
+		return LOADING_STATES[GuiGameStartFsmManager:get_field("<GameStartState>k__BackingField")] 
+	end
+	return false
+end
+
+local function isTitleSkip(retval)
+	if isLoading() then
+		return sdk.to_ptr(1)
+	end
+	return retval
+end
+
+local function notifyActionEnd(args)
+	sdk.to_managed_object(args[3]):call("notifyActionEnd")
+end
+
+local function playMovie(args)
+	if isLoading() then
+		local movie = sdk.to_managed_object(args[2])
+		if movie then
+			movie:seek(movie:get_DurationTime())
+		end
+	end
+end
+
+local function ClearFadeWithAction(args)
+	sdk.to_managed_object(args[3]):call("notifyActionEnd")
+	local FadeManager = sdk.get_managed_singleton("snow.SnowSingletonBehaviorRoot`1<snow.FadeManager>")
+	if FadeManager then 
+		FadeManager = FadeManager:get_field("_Instance") 
+		if FadeManager then
+			FadeManager:set_field("<FadeMode>k__BackingField", FINISHED)
+			FadeManager:set_field("fadeOutInFlag",false)
+		end
+	end
+end
+local function ClearFade(args)
+	local FadeManager = sdk.get_managed_singleton("snow.SnowSingletonBehaviorRoot`1<snow.FadeManager>")
+	if FadeManager then 
+		FadeManager = FadeManager:get_field("_Instance") 
+		if FadeManager then
+			FadeManager:set_field("<FadeMode>k__BackingField", FINISHED)
+			FadeManager:set_field("fadeOutInFlag",false)
+		end
+	end
+end
+
+function module.init()
+	config = require "RiseEnhanced.misc.config"
+	modUtils = require "RiseEnhanced.utils.mod_utils"
+	settings = modUtils.getConfigHandler({
+		intro = true,
+		online = true,
+	}, config.folder .. "/" .. module.folder)
+
+	LOADING_STATES = {
+		[sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsmManager.GameStartStateType"):get_field("Health_Caution"):get_data()] = true, -- 6
+		[sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsmManager.GameStartStateType"):get_field("CAPCOM_Logo"):get_data()] = true, -- 1
+		[sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsmManager.GameStartStateType"):get_field("Blank"):get_data()] = true, -- 5
+		[sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsmManager.GameStartStateType"):get_field("Re_Logo"):get_data()] = true -- 2
+	}
+
+	FINISHED = sdk.find_type_definition("snow.FadeManager.MODE"):get_field("FINISH"):get_data()
+
+	if settings.data.intro then
+		-- Fast forward movies to the end to mute audio
+		sdk.hook(sdk.find_type_definition("via.movie.Movie"):get_method("play"),
+		playMovie, function(ret) return ret end)
+
+		-- clear fadeout
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_CautionFadeIn"):get_method("update"),
+		ClearFade, function(ret) return ret end)
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_CAPCOMLogoFadeIn"):get_method("update"),
+		ClearFade, function(ret)return ret end)
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_HealthCautionFadeIn"):get_method("update"),
+		ClearFade, function(ret)return ret end)
+
+		-- Actual skip actions
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_RELogoFadeIn"):get_method("update"),
+		ClearFadeWithAction, function(ret)return ret end)
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_OtherLogoFadeIn"):get_method("update"),
+		ClearFadeWithAction, function(ret)return ret end)
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiGameStartFsm_AutoSaveCaution_Action"):get_method("start"),
+		notifyActionEnd, function(ret) return ret end)
+		sdk.hook(sdk.find_type_definition("snow.gui.fsm.title.GuiTitleFsm_PressAnyButton_Action"):get_method("start"),
+		notifyActionEnd, function(ret) return ret end)
+
+		-- Fake title skip input for HEALTH/Capcom
+		sdk.hook(sdk.find_type_definition("snow.gui.StmGuiInput"):get_method("getTitleDispSkipTrg"),
+		function(args)end, isTitleSkip)
+	end
+
+	if settings.data.online then
+		sdk.hook(sdk.find_type_definition("snow.SnowSessionManager"):get_method("reqOnlineWarning()"), function() return sdk.PreHookResult.SKIP_ORIGINAL end, function() end)
+	end
+end
+
+function module.draw()
+	if imgui.tree_node(config.lang.skips.name) then
+		settings.imgui("intro", imgui.checkbox, config.lang.skips.intro)
+		settings.imgui("online", imgui.checkbox, config.lang.skips.online)
+		imgui.text(config.lang.restartNote)
+		imgui.tree_pop()
+	end
+end
+
+return module
