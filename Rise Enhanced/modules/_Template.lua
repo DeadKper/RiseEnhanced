412499888d746c8370a080056f49d8385113161a
Missael Gatelum <35360914+DeadKper@users.noreply.github.com>
2023-02-23 16:18:50 -0700

    update template

--- Rise Enhanced/modules/_Template.lua
+++ Rise Enhanced/modules/_Template.lua
@@ -1,5 +1,6 @@
 local mod = require("Rise Enhanced.utils.mod")
 local utils = require("Rise Enhanced.utils.utils")
+local data = utils.getData()
 
 local module, settings, cache = mod.getDefaultModule(
     "Template", {
